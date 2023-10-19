b6ce358b568823eaf98862219cfd7cfd3cb39d80
Chris Ren <ren.chris@outlook.com>
2023-10-18 18:13:20 -0700

    refactor: remove unused base64urlToBuffer function

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,6 @@
 'use server'
 
 import crypto from 'crypto'
-import { Base64urlString } from '@github/webauthn-json/dist/types/base64url'
 
 export async function clean(str: string) {
   return str.replace(/\+/g, '-').replace(/\//g, '_').replace(/=/g, '')
@@ -19,16 +18,4 @@ export async function binaryToBase64url(bytes: Uint8Array) {
   })
 
   return btoa(str)
-}
-
-export async function base64urlToBuffer(base64urlString: Base64urlString): Promise<Buffer> {
-  let padding = base64urlString.length % 4
-  let modifiedBase64urlString = base64urlString
-
-  if (padding !== 0) {
-    padding = 4 - padding
-    modifiedBase64urlString += '==='.slice(0, padding)
-  }
-
-  return Buffer.from(modifiedBase64urlString.replace(/-/g, '+').replace(/_/g, '/'), 'base64')
 }
